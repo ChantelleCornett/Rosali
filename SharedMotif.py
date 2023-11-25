a1167e5cf136a35a5cdfbdb16b2c149e7c86b55d
ChantelleCornett <146009783+ChantelleCornett@users.noreply.github.com>
2023-11-25 14:53:03 +0000

    updated SharedMotif

--- SharedMotif.py
+++ SharedMotif.py
@@ -3,7 +3,8 @@
 f = open("/Users/m13477cc/Documents/demofile.txt", "r")
 
 input = f.readlines()
-
+with_s = len([x for x in input if x.startswith('>')])
+print(with_s)
 dna_strs = []
 
 for i in range(1,len(input),2):
@@ -11,9 +12,21 @@ for i in range(1,len(input),2):
         input[i]=input[i].replace('\n', '') 
     dna_strs.append(input[i])
 
-match_list = []
-for i in range(0,len(dna_strs)):
-    for j in range(1,len(dna_strs[i])):
-        for k in range(0, len(dna_strs[i])):
-        
-# start from biggest
+min_len_str = min(dna_strs, key = len)
+
+count = 0
+
+for j in range (len(min_len_str),1,-1):
+    for k in range(0,len(min_len_str) - j, 1):
+        count = 0
+        for i in dna_strs:
+            if i.find(min_len_str[k:k+j]) > -1: 
+                count += 1
+            next
+            if count == with_s:
+                print('max string: ', min_len_str[k:k+j])
+                if 1:
+                    break
+            
+            
+
