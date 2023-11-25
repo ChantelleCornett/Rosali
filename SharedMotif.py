7f74785afc2e67c160e8a446aad3cade6b2fa7bd
ChantelleCornett <146009783+ChantelleCornett@users.noreply.github.com>
2023-11-25 23:28:56 +0000

    update shared motif with new code

--- SharedMotif.py
+++ SharedMotif.py
@@ -1,10 +1,11 @@
 # Return largest commmon substring of the collection
+from difflib import SequenceMatcher
 
 f = open("/Users/m13477cc/Documents/demofile.txt", "r")
 
 input = f.readlines()
 with_s = len([x for x in input if x.startswith('>')])
-print(with_s)
+
 dna_strs = []
 
 for i in range(1,len(input),2):
@@ -14,19 +15,23 @@ for i in range(1,len(input),2):
 
 min_len_str = min(dna_strs, key = len)
 
-count = 0
+result = ""
+ # Iterate through each position in the strings up to the length of the shortest string
+for k in range(len(min_len_str)):
 
-for j in range (len(min_len_str),1,-1):
-    for k in range(0,len(min_len_str) - j, 1):
-        count = 0
-        for i in dna_strs:
-            if i.find(min_len_str[k:k+j]) > -1: 
-                count += 1
-            next
-            if count == with_s:
-                print('max string: ', min_len_str[k:k+j])
-                if 1:
-                    break
-            
-            
+    # Consider substrings of decreasing length at position i
+    for j in range(len(min_len_str) + 1, k-2, -1):
+
+        # Extract the substring from the first string
+        substring = dna_strs[0][k:j+1]
+        print(substring)
+        # Check if the substring is present in all other strings
+        if all(substring in s for s in dna_strs[1:]):
+
+            # Update the result if the current substring is longer
+            if len(substring) > len(result):
+                result = substring
+
+
+
 
